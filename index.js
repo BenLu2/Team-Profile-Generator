0e4d44b71e30473c6cd31f49b91874d8ed62dede
BenLu2 <90216349+BenLu2@users.noreply.github.com>
2021-12-06 23:19:03 -0800

    new

---
+++ index.js
@@ -0,0 +1,25 @@
+var team = [{name:'tom'}, [{name:kevin}]]
+var cards = []
+
+for (let i = 0; i < array.length; i++) {
+    var card = `
+    <h1> MY name is! ${team[i].name} </h1>
+    `
+    cards.push(card)
+}
+
+var fakeHtml = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    ${cards}
+</body>
+</html>`
+
+console.log('html!!!', fakeHtml)
