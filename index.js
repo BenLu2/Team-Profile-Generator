5405253bb39e0535a427c6ff81dff2de0c124688
Ben Lu <yudonglv@gmail.com>
2022-03-03 14:56:36 -0800

    changing inquerier input

--- index.js
+++ index.js
@@ -1,25 +1,167 @@
-var team = [{name:'tom'}, [{name:kevin}]]
-var cards = []
+const generateHtml = require('./classes/generateHtml')
+const inquirer = require('inquirer')
+const fs = require('fs')
+const Employee = require('./classes/employee')
+const Engineer = require('./classes/engineer')
+const Intern = require('./classes/intern')
+const Manager = require('./classes/manager')
+// const { template } = require('lodash')
+// console.log(generateHtml(team))
 
-for (let i = 0; i < array.length; i++) {
-    var card = `
-    <h1> MY name is! ${team[i].name} </h1>
-    `
-    cards.push(card)
+const team = [];
+
+const addManager = () => {
+
+return inquirer.prompt(
+    [
+        {
+            name: "name",
+            type: 'input',
+            message: "What is the manager name?",
+            validate: (response) => {
+                if (!response) {
+                    return console.log('manager name is required.')
+                } else {
+                    return true
+                }
+            }
+
+        },
+        {
+            name: "id",
+            type: 'input',
+            message: "What is this manager's id?",
+            validate: (response) => {
+                if (!response) {
+                    return console.log('manager id is required.')
+                } else {
+                    return true
+                }
+            }
+
+        },
+        {
+            name: "email",
+            type: 'input',
+            message: "What is the manager's email?",
+            validate: (response) => {
+                if (!response) {
+                    return console.log('manager email is required.')
+                } else {
+                    return true
+                }
+
+            },
+        },
+        {
+            name: "officeNumber",
+            type: 'input',
+            message: "What is manager's office number?",
+            validate: (response) => {
+                if (!response) {
+                    return console.log('office number is required.')
+                } else {
+                    return true
+                }
+
+            },
+
+        }
+    ]
+).then(managerInput => {
+    const  { name, id, email, officeNumber } = managerInput; 
+    const manager = new Manager (name, id, email, officeNumber);
+
+    teamArray.push(manager); 
+    console.log(manager); 
+})
+};
+
+const addEmployee = () => {
+    return inquirer.prompt(
+        [
+            {
+                name: "name",
+                type: 'input',
+                message: "What is the new employee's name?",
+                validate: (response) => {
+                    if (!response) {
+                        return console.log('office number is required.')
+                    } else {
+                        return true
+                    }
+    
+                },
+    
+            },
+            {
+                name: 'role',
+                type: 'list',
+                message: "Please choose your employee's role",
+                choices: ['Engineer', 'Intern']
+    
+            },
+            {
+                name: "id",
+                type: 'input',
+                message: "What is the new employee's id?",
+                validate: (response) => {
+                    if (!response) {
+                        return console.log('office number is required.')
+                    } else {
+                        return true
+                    }
+    
+                },
+    
+            },
+            {
+                name: "email",
+                type: 'input',
+                message: "What is new employee's email?",
+                validate: (response) => {
+                    if (!response) {
+                        return console.log('email is required.')
+                    } else {
+                        return true
+                    }
+    
+                },
+    
+            },
+
+            {
+                name: "github",
+                type: 'input',
+                message: "What is new employee's github?",
+                validate: (response) => {
+                    if (!response) {
+                        return console.log('github input is required.')
+                    } else {
+                        return true
+                    }
+    
+                },
+    
+            },
+
+            {
+                name: "school",
+                type: 'input',
+                message: "What is new intern's school?",
+                validate: (response) => {
+                    if (!response) {
+                        return console.log('school is required.')
+                    } else {
+                        return true
+                    }
+    
+                },
+    
+            }
+        ]
+    ).then(function(addAnother)
+    {
+})
+
 }
-
-var fakeHtml = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    ${cards}
-</body>
-</html>`
-
-console.log('html!!!', fakeHtml)
